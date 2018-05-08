dfa01c522ffc5b52f3b878b87cf1f73df1e99640
hsuyuming <abego452@gmail.com>
2018-05-08 20:40:34 +0800

    Updated status for enum

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :blogs
   
-  root to: 'pages#home'
+  #root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
