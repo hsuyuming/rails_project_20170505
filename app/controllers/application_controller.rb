6ae12e32548e037a6268aee11973973b5c865bd7
hsuyuming <abego452@gmail.com>
2018-05-14 22:19:59 +0800

    add additional annotate about :sign_up and :account_update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
+    #:sign_up & :account_update provided in the Devise source code.
+    #github:https://github.com/plataformatec/devise/blob/88724e10adaf9ffd1d8dbfbaadda2b9d40de756a/lib/devise/parameter_sanitizer.rb#L39
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
     devise_parameter_sanitizer.permit(:account_update, keys:[:name])
   end
